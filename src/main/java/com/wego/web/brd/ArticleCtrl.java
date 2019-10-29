799bc65aa7dcff4c5b4585b61e21c813c61cbc9e
eunb1115 <eunb94@gmail.com>
2019-10-29 20:18:07 +0900

    2019-10-29 20:18

--- src/main/java/com/wego/web/brd/ArticleCtrl.java
+++ src/main/java/com/wego/web/brd/ArticleCtrl.java
@@ -1,5 +1,6 @@
 package com.wego.web.brd;
 
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wego.web.cmm.IConsumer;
+import com.wego.web.cmm.IFunction;
+import com.wego.web.cmm.ISupplier;
 import com.wego.web.utl.Printer;
 
 
@@ -30,6 +33,8 @@ public class ArticleCtrl {
 	Printer printer;
 	@Autowired
 	ArticleMapper articleMapper;
+	@Autowired
+	List<Article> list;
 	
 	@PostMapping("/")
 	public Map<?, ?> write(@RequestBody Article param){
@@ -37,23 +42,55 @@ public class ArticleCtrl {
 		param.setBoardType("게시판");
 		IConsumer<Article> c = t ->articleMapper.insertArticle(param);
 		c.accept(param);
-		map.clear();
+		map.clear();	
 		map.put("msg", "SUCCESS");
+		ISupplier<String> s =()->articleMapper.countByArticle();
+		printer.accept("카운팅 : "+s.get());
+		map.put("count", s.get());
+		printer.accept("글쓰기 나옴");
 		return map;
 	}
-	@GetMapping("/{artseq}")
-	public Article searchByArticle(@PathVariable String artseq, @RequestBody Article param) {
-		return null;
+	@GetMapping("/")
+	public List<Article> list(){
+		list.clear();	
+		ISupplier<List<Article>> s = ()-> articleMapper.selectAll();
+		printer.accept("전체 글목록 \n"+s.get());
+		
+		return s.get();
 		
 	}
+	
+	@GetMapping("/count")
+	public Map<?,?> count(){
+		ISupplier<String> s =()->articleMapper.countByArticle();
+		printer.accept("카운팅 : "+s.get());
+		map.clear();
+		map.put("count", s.get());
+		return map;
+		
+	}
+	
 	@PutMapping("/{artseq}")
-	public Article updateArticle(@PathVariable String artseq, @RequestBody Article param) {
-		return null;
+	public Map<?,?> updateArticle(@PathVariable String artseq, @RequestBody Article param) {	
+		param.setArtseq(artseq);
+		printer.accept("수정들어옴 "+artseq);
+		IConsumer<Article> c=t-> articleMapper.updateArticle(param);
+		
+		c.accept(param);
+		map.clear();
+		map.put("msg", "SUCCESS");
+		printer.accept("수정나감 "+param.toString());
+		return map;
 		
 	}
 	@DeleteMapping("/{artseq}")
-	public Article removeArticle(@PathVariable String artseq, @RequestBody Article param) {
-		return null;
+	public Map<?,?> deleteArticle(@PathVariable String artseq, @RequestBody Article param) {
+		printer.accept("삭제들어옴 "+artseq);
+		IConsumer<Article> c=t-> articleMapper.deleteArticle(param);
+		c.accept(param);
+		map.clear();
+		map.put("msg", "SUCCESS");
+		return map;
 	}
 	
 	
--- src/main/java/com/wego/web/brd/ArticleMapper.java
+++ src/main/java/com/wego/web/brd/ArticleMapper.java
@@ -1,9 +1,15 @@
 package com.wego.web.brd;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleMapper {
 	public void insertArticle(Article article);
+	public String countByArticle();
+	public List<Article> selectAll();
+	public void updateArticle(Article article);
+	public void deleteArticle(Article article);
 
 }
